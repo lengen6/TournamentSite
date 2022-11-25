7599f22c37f9017c7fd83646ffa078962bef95bf
Michael <lengen.6@gmail.com>
2022-11-25 17:57:46 -0500

    javascript integration working

---
+++ wwwroot/js/Match.js
@@ -0,0 +1,16 @@
+﻿
+const redScoreIncrease = document.querySelector(".Plus-Red");
+const redScoreDecrease = document.querySelector(".Minuse-Red");
+const blueScoreIncrease = document.querySelector(".Plus-Blue");
+const blueScoreDecrease = document.querySelector(".Minus-Blue");
+const redScoreDisplay = document.querySelector(".Points-Red");
+const blueScoreDisplaye = document.querySelector(".Points-Blue");
+let blueScore = 0;
+let redScore = 0;
+
+redScoreIncrease.addEventListener('click', () => {
+    redScore++;
+    redScoreDisplay.textContent = redScore.toString();
+})
+
+document.getElementById("<%=RedScore.ClientID%>").value = "87";
