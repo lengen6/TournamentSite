6091f41199c2f65b2d2356714e335a0a9b571313
Michael <lengen.6@gmail.com>
2022-11-27 20:03:16 -0500

    Match results search working. MVP achieved.

--- Pages/Events/History.cshtml.cs
+++ Pages/Events/History.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Build.Graph;
 using Microsoft.EntityFrameworkCore;
 using TieRenTournament.Models;
 
@@ -7,6 +8,19 @@ namespace TieRenTournament.Pages.Events
 {
     public class HistoryModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public string CompetitorFirstNameSearch { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CompetitorLastNameSearch { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string WinnerFirstNameSearch { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string WinnerLastNameSearch { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int PointsSearch { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string VictoryMethodSearch { get; set; }
+
         private readonly TieRenTournament.Data.ApplicationDbContext _context;
 
         public HistoryModel(TieRenTournament.Data.ApplicationDbContext context)
@@ -18,6 +32,42 @@ namespace TieRenTournament.Pages.Events
         public void OnGet()
         {
             Matches = _context.Match.Include(r => r.CompeitorRed).Include(b => b.CompeitorBlue).ToList();
+
+            if(CompetitorFirstNameSearch != null)
+            {
+                Matches = Matches.Where(m => m.CompeitorRed.FirstName.Contains(CompetitorFirstNameSearch)
+            || m.CompeitorBlue.FirstName.Contains(CompetitorFirstNameSearch)).ToList();
+            }
+
+            if (CompetitorLastNameSearch != null)
+            {
+                Matches = Matches.Where(m => m.CompeitorRed.LastName.Contains(CompetitorLastNameSearch)
+            || m.CompeitorBlue.LastName.Contains(CompetitorLastNameSearch)).ToList();
+            }
+
+            if (WinnerFirstNameSearch != null)
+            {
+                Matches = Matches.Where(m => m.Winner.FirstName.Contains(WinnerFirstNameSearch)).ToList();
+            }
+
+            if (WinnerLastNameSearch != null)
+            {
+                Matches = Matches.Where(m => m.Winner.LastName.Contains(WinnerLastNameSearch)).ToList();
+            }
+            if (PointsSearch != null)
+            {
+                Matches = Matches.Where(m => m.CompetitorRedScore >= PointsSearch
+            || m.CompetitorBlueScore >= PointsSearch).ToList();
+            }
+
+            if(VictoryMethodSearch != null)
+            {
+                Matches = Matches.Where(m => m.VictoryMethod.Contains(VictoryMethodSearch)).ToList();
+            }
         }
     }
 }
+
+//Where(m => m.CompeitorRed.FirstName.Contains(CompetitorFirstNameSearch)
+//            || m.CompeitorBlue.FirstName.Contains(CompetitorFirstNameSearch) || m.CompeitorRed.LastName.Contains(CompetitorLastNameSearch) ||
+//            m.CompeitorBlue.LastName.Contains(CompetitorLastNameSearch))
